3ca7722bfee211d81eea04bf4f7a8cc295bcbf3d
BlasCasale <casale.blas@live.com>
2024-05-02 19:15:20 -0300

    termine con el hexa, empaquetado y desempaquetado

--- OC/index.js
+++ OC/index.js
@@ -79,8 +79,7 @@ const getBinary = (string, system) => {
               boolean = !boolean;
               result = result + Math.pow(2, exponent - i);
             }
-          }
-          if (i == 0) result = result * (-1);
+          } else if (i == 0) result = result * (-1);
         } else {
           if ((string[i] == '1') && (i > 0)) {
             result = result + (Math.pow(2, exponent - i))
@@ -244,3 +243,209 @@ createBinary('42');
 createBinary('-42');
 createBinary('-127');
 createBinary('-128');
+
+const getHexa = (binary) => {
+  const hexa = {
+    '0000': '0',
+    '0001': '1',
+    '0010': '2',
+    '0011': '3',
+    '0100': '4',
+    '0101': '5',
+    '0110': '6',
+    '0111': '7',
+    '1000': '8',
+    '1001': '9',
+    '1010': 'A',
+    '1011': 'B',
+    '1100': 'C',
+    '1101': 'D',
+    '1110': 'E',
+    '1111': 'F'
+  };
+
+  let result = '';
+
+  const rest = binary.length % 4;
+
+  if (rest == 0) {
+    const roundsOfFor = binary.length / 4;
+
+    for (let i = 1; i <= roundsOfFor; i++) {
+      const cuttedChain = binary.slice(0, 4);
+      result += hexa[cuttedChain];
+      binary = binary.slice(4);
+    };
+  } else {
+    const finalLength = binary.length + (4 - rest);
+
+    binary = binary.padStart(finalLength, '0');
+
+    const roundsOfFor = binary.length / 4;
+
+    for (let i = 1; i <= roundsOfFor; i++) {
+      const cuttedChain = binary.slice(0, 4);
+      result += hexa[cuttedChain];
+      binary = binary.slice(4);
+    };
+  };
+
+  return result;
+};
+
+
+console.log(getHexa('1010101010010011'))
+console.log(getHexa('0011111'))
+
+const convertHexa = (hexa) => {
+  const binary = {
+    '0': '0000',
+    '1': '0001',
+    '2': '0010',
+    '3': '0011',
+    '4': '0100',
+    '5': '0101',
+    '6': '0110',
+    '7': '0111',
+    '8': '1000',
+    '9': '1001',
+    'A': '1010',
+    'B': '1011',
+    'C': '1100',
+    'D': '1101',
+    'E': '1110',
+    'F': '1111'
+  };
+
+  let result = '';
+
+  for (let i = 0; i < hexa.length; i++) {
+    result += binary[hexa[i]];
+  }
+
+  return result;
+}
+
+console.log(convertHexa('AA'))
+
+console.log(convertHexa('2B1'))
+
+const BCDempSS = (number) => {
+  const chain = number.toString();
+
+  const numbers = {
+    '0': '0000',
+    '1': '0001',
+    '2': '0010',
+    '3': '0011',
+    '4': '0100',
+    '5': '0101',
+    '6': '0110',
+    '7': '0111',
+    '8': '1000',
+    '9': '1001'
+  };
+
+  let result = '';
+
+  for (let i = 0; i < chain.length; i++) {
+    result += numbers[chain[i]];
+  };
+
+  return result;
+};
+
+console.log(BCDempSS(89))
+console.log(BCDempSS(14))
+
+const BCDempCS = (chain) => {
+  const numbers = {
+    '0': '0000',
+    '1': '0001',
+    '2': '0010',
+    '3': '0011',
+    '4': '0100',
+    '5': '0101',
+    '6': '0110',
+    '7': '0111',
+    '8': '1000',
+    '9': '1001'
+  };
+
+  let result = '';
+
+  for (let i = 1; i < chain.length; i++) {
+    result += numbers[chain[i]];
+  };
+
+  if (chain[0] == '-') result += '1101';
+  else result += '1100';
+
+  return result;
+};
+
+console.log(BCDempCS('-59'));
+console.log(BCDempCS('+59'));
+
+const BCDdesSS = (number) => {
+  const chain = number.toString();
+
+  const numbers = {
+    '0': '0000',
+    '1': '0001',
+    '2': '0010',
+    '3': '0011',
+    '4': '0100',
+    '5': '0101',
+    '6': '0110',
+    '7': '0111',
+    '8': '1000',
+    '9': '1001'
+  };
+
+  let result = '';
+
+  for (let i = 0; i < chain.length; i++) {
+    result += `1111${numbers[chain[i]]}`;
+  };
+
+  return result;
+}
+
+console.log(BCDdesSS(89));
+
+const BCDdesCS = (number) => {
+
+  let chain = number.toString();
+
+  chain = chain.slice(1);
+  
+  const numbers = {
+    '0': '0000',
+    '1': '0001',
+    '2': '0010',
+    '3': '0011',
+    '4': '0100',
+    '5': '0101',
+    '6': '0110',
+    '7': '0111',
+    '8': '1000',
+    '9': '1001'
+  };
+
+  let result = '';
+
+  for (let i = 0; i < chain.length; i++) {
+    if (i == chain.length - 1) {
+      if (number < 0) result += `1101${numbers[chain[i]]}`;
+      else result += `1100${numbers[chain[i]]}`;
+    } else {
+      result += `1111${numbers[chain[i]]}`;
+    };
+  };
+
+  return result;
+};
+
+console.log(BCDdesCS('-89'), ' bcd des cs');
+console.log(BCDdesCS('+89'), ' bcd des cs');
