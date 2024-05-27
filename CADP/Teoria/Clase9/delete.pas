3b5e94092eeef4729aaa7bbd82ee70f4ba3bab96
BlasCasale <casale.blas@live.com>
2024-05-27 16:02:46 -0300

    lista la teoria

---
+++ CADP/Teoria/Clase9/delete.pas
@@ -0,0 +1,56 @@
+program delete;
+
+type
+  list = ^node;
+
+  node = record
+    element: integer;
+    next: list;
+  end;
+
+procedure deleteNode (var l: list; value: integer);
+var
+  prev, current: list;
+begin
+  current:= l;
+  while ((current <> nil) and (current^.element <> value)) do
+    begin
+      prev:= current;
+      current:= current^.next;
+    end;
+
+  if (current <> nil) then
+    begin
+      if (current = l) then
+        l:= l^.next
+      else
+        prev^.next:= current^.next;
+      dispose(current);
+    end;
+end;
+procedure deleteVariousNodes (var l: list; value: integer);
+var
+  prev, current: list;
+begin
+  current:= l;
+  while (current <> nil) do
+    begin
+      if (current^.element <> value) then
+        begin
+          prev:= current;
+          current:= current^.next;
+        end
+      else
+        begin
+          if (current = l) then
+            l:= l^.next
+          else
+            prev^.next:= current^.next;
+          dispose(current);
+          current:= prev;
+        end;
+    end;
+end;
+begin
+  {Programa principal}
+end.
---
+++ CADP/Teoria/Clase9/insertion.pas
@@ -0,0 +1,63 @@
+program insertion;
+
+type
+  list = ^node;
+
+  node = record
+    element: integer;
+    next: list;
+  end;
+
+  {
+    cuando se inserta, se va a insertar dentro de una lista que este ordenada pero quiero que persista ese orden
+  }
+procedure insertNode (var l: list; value: integer);
+var
+  prev, current, newNode: node;
+begin
+  new(newNode);
+  newNode^.element:= value;
+  newNode^.next:= nil;
+
+  if (l = nil) then
+    l:= newNode
+  else
+    begin
+      prev:= l;
+      current:= l;
+
+      while ((current <> nil) and (current^.element < newNode^.element)) do
+        begin
+          prev:= current;
+          current:= current^.next;
+        end;
+    end;
+  
+  if (current = l) then
+    begin
+      newNode^.next:= l;
+      l:= newNode;
+    end
+  else
+    begin
+      prev^.next:= newNode;
+      newNode^.next:= current;
+    end;
+
+  // if (current = l) then {otra version}
+  //   begin
+  //     newNode^.next:= l;
+  //     l:= newNode;
+  //   end
+  // else if (current <> nil) then
+  //   begin
+  //     prev^.next:= newNode;
+  //     newNode^.next:= current;
+  //   end
+  // else
+  //   prev^.next:= newNode;
+  
+end;
+begin
+  {Programa principal}
+end.
