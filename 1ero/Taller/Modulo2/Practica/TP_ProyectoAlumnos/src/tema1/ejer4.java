0d6f683ee9d2ca549db83eda3e8de4da4a9b472f
BlasCasale <casale.blas@live.com>
2024-09-25 00:37:54 -0300

    minuto 19

--- 1ero/Taller/Modulo2/Practica/TP_ProyectoAlumnos/src/tema1/ejer4.java
+++ 1ero/Taller/Modulo2/Practica/TP_ProyectoAlumnos/src/tema1/ejer4.java
@@ -12,7 +12,43 @@ import PaqueteLectura.Lector;
  */
 public class ejer4 {
 
-    public static void main(String[] args) {
+  /*
+  Un edificio de oficinas está conformado por 8 pisos(1..8) y 4 oficinas por piso (1..4). 
+  Realice un programa que permita informar la cantidad de personas que
+  concurrieron a cada oficina de cada piso. Para esto, simule la llegada de personas al
+  edificio de la siguiente manera: a cada persona se le pide el nro. de piso y nro. de
+  oficina a la cual quiere concurrir. La llegada de personas finaliza al indicar un nro.
+  de piso 9. Al finalizar la llegada de personas, informe lo pedido.
+   */
+  public static void main(String[] args) {
+    int cut = 9;
+    int floors = 8;
+    int offices = 4;
+    int floor, office;
+    int[][] building = new int[floors][offices];
+    int i, j;
 
+    System.out.println("Ingrese un piso: ");
+    floor = Lector.leerInt();
+    if (floor != cut) {
+      System.out.println("Ingrese la oficina: ");
+      office = Lector.leerInt();
+      
+      while (floor != cut) {
+        building[floor - 1][office - 1] = building[floor - 1][office - 1] + 1;
+
+        System.out.println("Ingrese un piso: ");
+        floor = Lector.leerInt();
+        
+        if (floor != cut) {
+          System.out.println("Ingrese la oficina: ");
+          office = Lector.leerInt();
+        }
+      }
     }
+    
+    for (i = 0; i < floors; i++) {
+      for (j= 0; j < offices; j++) System.out.println("Piso " + (i + 1) + " y oficina " + (j + 1) + " fueron " + building[i][j] + " personas.");
+    }
+  }
 }
