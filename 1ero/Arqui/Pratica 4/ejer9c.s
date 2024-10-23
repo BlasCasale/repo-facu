002ee44950cb2388532b6a25d647664862f8afb5
Blas Casale <117787465+BlasCasale@users.noreply.github.com>
2024-10-23 09:43:41 -0300

    Add files via upload

--- 1ero/Arqui/Pratica 4/ejer9c.s
+++ 1ero/Arqui/Pratica 4/ejer9c.s
@@ -1,3 +1,24 @@
-; c) Contar mayúsculas Escribir un programa que cuente la cantidad de letras mayúsculas de un string. Probarlo con el
-; string “ArquiTectuRa de ComPutaDoras”. Pista: El código ASCII de la “A” es 65, y el de la “Z” es 90
+;Contar mayúsculas Escribir un programa que cuente la cantidad de letras mayúsculas de un string. Probarlo con el
+; string “ArquiTectuRa de ComPutaDoras”. Pista: El código ASCII de la “A” es 65, y el de la “Z” es 90.
 
+.data
+str: .asciiz "ArquiTectuRa de ComPutaDoras"
+control: .word 91
+quantity: .word 0
+.code 
+dadd $t6, $0, $0 ; contdor
+daddi $t7, $0, str ; direccion
+daddi $t8, $0, 8 ; desplazamiento por reg 
+ld $t9, control($0) ; el proximo a la ult mayus
+while: ld $t0, 0($t7) 
+cutControl: andi $t1, $t0, 0x00000000000000FF ; and para quedarme con el ultimo del str
+beqz $t1, toEnd
+dsrl $t0, $t0, 8 ; corrimiento para seguir
+slt $t2, $t1, $t9 ; si queda 1 $t1 es mayus
+bnez $t2, continue
+daddi $t6, $t6, 1
+continue: bnez $t0, cutControl
+daddi $t7, $t8, $t7
+j while
+sd $t6, quantity($0)
+halt
