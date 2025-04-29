3f4d83bad3c094aa2afbee9ae53b53e43143908d
BlasCasale <casale.blas@live.com>
2025-04-29 14:20:35 -0300

    termine practica 3 de fod

--- 2do/FOD/Práctica/Practica3/Parte2/ejer1.pas
+++ 2do/FOD/Práctica/Practica3/Parte2/ejer1.pas
@@ -19,6 +19,93 @@ type
   master = file of product;
 
   sells = file of sell;
+
+  list = ^node;
+
+  node = record
+    data: sell;
+    next: list;
+  end;
+
+procedure readSell (var a: sells; var s: sell);
+begin
+  if (not eof (a)) then
+    read(a, s)
+  else
+    s.id:= highValue;
+end;
+
+procedure chargeNode (var l: list; s: sell);
+var
+  newNode, prev, act: list;
+begin
+
+  if (l = nil) then
+    begin
+      new(newNode);
+      newNode^.data:= s;
+      newNode^.next:= nil;
+      l:= newNode;
+    end
+  else
+    begin
+      act:= l;
+      while ((act <> nil) and (act^.data.id < s.id)) do
+        begin
+          prev:= act;
+          act:= act^.next;
+        end;
+
+      if (act^.data.id = s.id) then
+        act^.data.quantity:= act^.data.quantity + s.quantity
+      else
+        begin
+          new(newNode);
+          newNode^.data:= s;
+          if (act = l) then
+            l:= newNode
+          else
+            prev^.next:= newNode;
+          newNode^.next:= act;
+        end;
+    end;
+end;
+
+procedure makeListWithSells (var s: sells; var l: list);
+var
+  aux: sell;
+begin
+  reset(s);
+
+  readSell(s, aux);
+  while (aux.id <> highValue) do
+    begin
+      chargeNode(l, aux);
+      readSell(s, aux);
+    end;
+
+  close(s);
+end;
+
+procedure actualizeMaster (var a: master; l: list);
+var
+  s: sell;
+begin
+  reset(a);
+
+  while (l <> nil) do
+    begin
+      read(a, s);
+      while (l^.data.id <> s.id) do
+        read(a, s);
+      s.quantity:= s.quantity - l^.data.quantity;
+      seek(a, filepos(a) - 1);
+      write(a, s);
+      seek(a, 0);
+    end;
+    
+  close(a);
+end;
 {
   El encargado de ventas de un negocio de productos de limpieza desea administrar el 
   stock de los productos que vende. Para ello, genera un archivo maestro donde figuran 
